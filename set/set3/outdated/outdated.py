daf9509ca86705bf4149c7add527cb07dc2d8fbb
Loremgreg <gregorybastin.de@gmail.com>
2025-11-17 16:18:53 +0100

    fin outdated

--- set/set3/outdated/outdated.py
+++ set/set3/outdated/outdated.py
@@ -20,52 +20,41 @@ month_list = [
     "December"
 ]
 
-
-# diviser input en 3 avec .split()
 def main():
-    
-    date_user_input = input("Date: ")
-    split_user = split(date_user_input)
-    dic_month_num = create_dic(split_user)
-    print(dic_month_num)
+    while True:
+        try:
+            date_user_input = input("Date: ")
+            year, month, day = parse_date(date_user_input)
+            print(f"{year}-{month:02}-{day:02}")
+            break
+        except ValueError:
+             pass
 
-    # print(split_user) # ['4', '5', '1999']
-    m, d, y = int(split_user[0]), int(split_user[1]), split_user[2]
-    # print(m, d, y) # 4 5 1999
-    print(f"{y}-{m:02}-{d:02}") # 1999-04-05
+def parse_date(date_user_input):
+        try:
+            if "/" in date_user_input:
+                month, day, year = date_user_input.split("/")
+                # separe 4 5 1999
 
+            elif "," in date_user_input:
+                month_day, year = date_user_input.split(", ")
+                 # Sépare "September" et  "8"
+                month_name, day = month_day.split(" ")
+                # month_list.index(month_name) trouve la position (qui commence à 0).
+                # On ajoute 1 car les mois commencent pas a 0.
+                month = month_list.index(month_name) + 1
 
+            elif not ("," or "/") in date_user_input:
+                 raise ValueError
 
-def split(date_user_input):
-    split_user_input = date_user_input.split('/')
-    split_list = []
-    for input in split_user_input:
-        split_list.append(input)
-    return split_list
+            day, month, year = int(day), int(month), int(year)
 
+            if not (1 <= month <= 12 and 1 <= day <= 31):
+                    raise ValueError
+            return year, month, day
 
-def create_dic(date_parts):
-    # This creates a dictionary mapping month names to numbers, e.g., {"January": 1, "February": 2, ...}
-    dic_month_list = {name: index for index, name in enumerate(month_list, start=1)}
-    # Get the month name, which is the first element of the list passed to the function.
-    month_name = date_parts[0]
-    # Use the extracted month name to look up its corresponding number in the dictionary.
-    value_of_month = dic_month_list.get(month_name)
-    return value_of_month
-
+        except (KeyError, ValueError):
+            raise ValueError("Invalid date")
 
 main()
 
-
-
-
-# prendre l'annee le mettre a la premiere position
-# prendre les month mettre a la 2 eme position
-# prendre les jours mettre a la 3 eme position
-
-
-# Replacing substrings with the <str>.replace(<old>, <new>) method
-# list method .index[] -> mettre chaque 3 parties dans 3 nouvelles liste -> re ecrire en format YYYY-MM-DD
-
-
-# print("Dates: {0}/{1}/{2}. format() )
