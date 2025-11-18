56fae5a8738805a3f92c1dc532831d77e1a1807e
Loremgreg <gregorybastin.de@gmail.com>
2025-11-18 13:28:13 +0100

    define fonction

---
+++ project/pathology/project.py
@@ -0,0 +1,37 @@
+
+def main():
+# gère le menu, les input(), les print()
+
+# Si tape :
+# 1. liste toutes les patho
+# 2. filtrer selon partie du corps: search_pathologies_by_body_part: 
+# 3. Recherche par nom (par slug) : get_pathology_by_slug()
+# 4. Recherche par mot clé:  search_pathologies_by_query()
+# 5. quitter 
+
+# print 
+# le menu interractif 
+# → plusieurs actions
+# → saisie utilisateur
+# → affichage formaté
+# → moteur de recherche simple
+ 
+
+def list_all_pathologies():
+
+def get_pathology_by_query():
+# Recherche simple par mot clé (ou partie du nom) 
+
+def get_pathology_by_slug():
+#  Retourne la pathologie dont le slug correspond
+
+def normalize_name():
+# insensible aux majuscules/minuscules, espaces   
+
+def search_pathologies_by_body_part():
+# 4. filtrer selon MS, MI, Dos, Traumatologie (fracture, elongation): search_pathologies_by_body_part: 
+
+
+
+
+
