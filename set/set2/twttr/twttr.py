a902b997e1b021ce8122288e4c76f2e6286849aa
Loremgreg <gregorybastin.de@gmail.com>
2025-11-23 19:52:27 +0100

    test unit

--- set/set2/twttr/twttr.py
+++ set/set2/twttr/twttr.py
@@ -1,17 +1,23 @@
 vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 
-tweet = input("Input: ")
-vowels_in_tweet = []
-consonne_in_tweet = []
+
+def main():
+    tweet = input("Input: ")
+    print(shorten(tweet))
 
 
-for letter in tweet:
-    if letter in vowels:
-        vowels_in_tweet.append(letter)
-    else:
-        consonne_in_tweet.append(letter)
+def shorten(tweet):
+    vowels_in_tweet = []
+    consonne_in_tweet = []
+    for letter in tweet:
+        if letter in vowels:
+            vowels_in_tweet.append(letter)
+        else:
+            consonne_in_tweet.append(letter)
 
-final_tweet = "".join(consonne_in_tweet)
+    final_tweet = "".join(consonne_in_tweet)
 
-print(final_tweet, end="")
+    return final_tweet
 
+if __name__ == "__main__":
+    main()
